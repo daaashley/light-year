199b38c190482f4c5045ca42fd7826f605017972
David Ashley <david-ashley@hotmail.com>
2024-05-20 20:32:55 -0500

    heap

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,8 +1,26 @@
 #include <vector>
 #include <iostream>
 
+void heap()
+{
+    int x = 5;               // x is created on the stack
+    int *heapX = new int{5}; // an int variable with value 5 is created on the heap, then addr is stored in the stack variable x
+}
+
+void array()
+{
+    int intArray[5] = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < sizeof(intArray) / sizeof(int); i++)
+    {
+        std::cout << intArray[i] << std::endl;
+    }
+}
+
 int main()
 {
+    heap();
+    array();
     int x = 5;
     int &anotherx = x; // a reference.
     anotherx = 10;
