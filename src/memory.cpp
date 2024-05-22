09971bbbffbcf46ade7b3eb35f059f0705d45e24
David Ashley <david-ashley@hotmail.com>
2024-05-22 16:00:27 -0500

    comment

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,10 +1,61 @@
 #include <vector>
 #include <iostream>
+#include <memory>
+
+void sharedPointer()
+{
+    std::shared_ptr<int> intSharedPtr = std::make_shared<int>(5);
+
+    // Does allow copy, The raw memory will be free, when and only when the last shared pointer that holds it is deleted.
+    // std::shared_ptr<int> anotherSharedPtr = intSharedPtr;
+
+    // If you want to manually clear the memory call
+
+    // intSharedPtr.reset();
+
+    // Weak ptr, empty init
+    std::weak_ptr<int> emptyWeakPtr;
+
+    // Shared ptr delete/reset cascades to weak_ptrs, and those allocations are clean up as well
+    // Weak ptr, creates weak ptr with same mem addr of the intShared Ptr
+    std::weak_ptr<int> intWeakPtr = intSharedPtr;
+
+    intSharedPtr.reset();
+
+    if (intWeakPtr.expired())
+    {
+        std::cout << "weak ptr expired" << std::endl;
+    }
+    else
+    {
+        std::cout << "weak ptr not expired" << std::endl;
+    }
+}
+
+void smartPointer()
+{
+    // Unique ptr - usage similar to raw ptr, automatically deletes or free's memory out of scope
+    std::unique_ptr<int> intUniquePtr = std::make_unique<int>(5);
+
+    std::unique_ptr<int> anotherUniquePtr = std::move(intUniquePtr);
+
+    std::cout << intUniquePtr << std::endl;
+}
 
 void heap()
 {
+    // "new" is heap
     int x = 5;               // x is created on the stack
     int *heapX = new int{5}; // an int variable with value 5 is created on the heap, then addr is stored in the stack variable x
+
+    delete heapX;    // I do not need to use where this pointer points anymore
+    heapX = nullptr; // This is saying heapX now does not point to anything
+
+    // Memory access violation
+    if (heapX != nullptr)
+    {
+        std::cout << *heapX << std::endl; // Hey nvm, I need it again
+    }
 }
 
 void array()
@@ -19,6 +70,8 @@ void array()
 
 int main()
 {
+    sharedPointer();
+    smartPointer();
     heap();
     array();
     int x = 5;
