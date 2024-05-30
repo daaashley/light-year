e3bc025540c7646205f79982c25ae4d03965f9b4
David Ashley <david-ashley@hotmail.com>
2024-05-29 20:43:58 -0500

    deconst

--- src/classes.cpp
+++ src/classes.cpp
@@ -4,7 +4,33 @@
 
 class Student
 {
-public: // anything under public is accessible from the outside
+public:                                                                     // anything under public is accessible from the outside
+    Student(const std::string &studentName, int studentID, int studentYear) // Constructor is func w no return type and same name as class.
+    // : name{studentName}, ID{studentID}, year{studentYear} is another way, more performant
+    {
+        name = studentName;
+        ID = studentID;
+        year = studentYear;
+        // empty constructor is default constructor
+    }
+
+    Student(const char *rawStringName, int studentID, int studentYear)
+        : name{rawStringName}, ID{studentID}, year{studentYear}
+    {
+    }
+
+    ~Student() // Called when object is out of scope or deleted if alloc'ed on the heap
+    {
+        // delete or free heap params/vars
+        std::cout << "student " << name << " is destructed" << std::endl;
+    }
+
+    void sayHello()
+    {
+        std::cout << name << " says hello" << std::endl;
+    }
+
+    // Member variables
     std::string name;
     int ID;
     int year;
@@ -15,12 +41,9 @@ private:
 
 int main()
 {
-    Student studentOne;
-    studentOne.name = "Sam";
-    studentOne.ID = 0001;
-    studentOne.year = 1;
+    Student studentOne{"Sam", 1234, 1};
 
-    Student studentTwo;
+    Student studentTwo{"Sam", 1234, 1};
     studentTwo.name = "Emily";
     studentTwo.ID = 0002;
     studentTwo.year = 2;
