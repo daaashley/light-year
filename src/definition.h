b2ec208f84e0f10020ece6ceca979fc2dd037369
David Ashley <david-ashley@hotmail.com>
2024-05-31 22:13:01 -0500

    project del, chg

--- src/definition.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef UTILITY_DLL_EXPORT
-#define UTILITY_API __declspec(dllexport)
-#else
-#define UTILITY_API __declspec(dllimport)
-#endif
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-#include "utility.h"
-
-int main()
-{
-    std::vector<int> randomNums = generateRandomNumbers(20);
-    sortVector(randomNums);
-    printVector(randomNums);
-}
